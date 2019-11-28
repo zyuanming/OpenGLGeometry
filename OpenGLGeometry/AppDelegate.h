3715a2c081083f050496aa8655ef6a7b9bbedefe
zyuanming <zyuanming@outlook.com>
2019-11-28 17:14:10 +0800

    first commit

---
+++ OpenGLGeometry/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGLGeometry
+//
+//  Created by Zhang on 2019/11/28.
+//  Copyright © 2019 Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
